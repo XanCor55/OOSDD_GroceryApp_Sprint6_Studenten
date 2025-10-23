4ea39a41e2e6166d7883354079a5bfa48bdeaf47
Xander <s1218452@student.windesheim.nl>
2025-10-23 15:39:21 +0200

    Boodschappenlijst namen dynamisch tonen in afreken scherm

--- Grocery.App/ViewModels/AfrekenenViewModel.cs
+++ Grocery.App/ViewModels/AfrekenenViewModel.cs
@@ -1,17 +1,23 @@
-﻿using Grocery.Core.Data.Repositories;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using Grocery.Core.Data.Repositories;
 using Grocery.Core.Interfaces.Repositories;
+using Grocery.Core.Interfaces.Services;
 using Grocery.Core.Models;
-using System;
-using System.Collections.Generic;
+using Grocery.Core.Services;
 using System.Collections.ObjectModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Grocery.App.ViewModels
 {
     public partial class AfrekenenViewModel : BaseViewModel
     {
-        
+        public ObservableCollection<GroceryList> GroceryLists { get; set; }
+        private readonly IGroceryListService _groceryListService;
+        public ObservableCollection<GroceryListItem> MyGroceryListItems { get; set; } = [];
+
+        public AfrekenenViewModel(IGroceryListService groceryListService)
+        {
+            _groceryListService = groceryListService;
+            GroceryLists = new(_groceryListService.GetAll());
+        }
     }
 }
